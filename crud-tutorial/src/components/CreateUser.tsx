9070feb41911664b49d01063dbacdc5a8cfa6526
Luiz Mainart <luizdmmainart@gmail.com>
2022-03-18 21:16:06 -0300

    CRUD Tutorial - Create User React

--- crud-tutorial/src/components/CreateUser.tsx
+++ crud-tutorial/src/components/CreateUser.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CreateUser() {
+  const [inputs, setInputs] = useState({})
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    console.log(inputs)
+  }
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget
+
+    setInputs(values => ({...values, [name]: value}))
+  }
+
   return (
-    <h1>Create User</h1>
+    <div>
+      <h1>Create User</h1>
+      <form onSubmit={handleSubmit}>
+        <table cellSpacing="10">
+          <tbody>
+            <tr>
+              <th>
+                <label htmlFor="name">Name: </label>
+              </th>
+              <td>
+                <input type="text" name="name" id="name" onChange={handleChange} />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="email">Email: </label>
+              </th>
+              <td>
+                <input type="email" name="email" id="email" onChange={handleChange} />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="mobile">Mobile:</label>
+              </th>
+              <td>
+                <input type="tel" name="mobile" id="mobile" onChange={handleChange} />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2} align="right">
+                <button type="submit">Save</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
   )
 }
